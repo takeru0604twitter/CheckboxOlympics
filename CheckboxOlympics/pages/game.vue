d0e8d966258066bc4474041077b619e783ab7d98
takeru0604twitter <takeru20030604@icloud.com>
2021-12-11 04:22:59 +0900

    index.vueの変更

---
+++ CheckboxOlympics/pages/game.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        this is game.vue
+    </div>
+</template>
+
+<script>
+</script>
--- CheckboxOlympics/pages/index.vue
+++ CheckboxOlympics/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <Tutorial/>
+  <div>
+    this is index.vue
+  </div>
 </template>
 
 <script>
