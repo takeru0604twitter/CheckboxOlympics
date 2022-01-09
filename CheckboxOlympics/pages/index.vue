352f8d4506f6ba817603bfc9c352a1aaf26e3a00
takeru0604twitter <takeru20030604@icloud.com>
2022-01-09 18:45:39 +0900

    boxの途中まで

--- CheckboxOlympics/pages/index.vue
+++ CheckboxOlympics/pages/index.vue
@@ -1,9 +1,90 @@
 <template>
+
   <div>
-    this is index.vue.
+    <div class="a">CheckB<input type="checkbox" checked>x <input type="checkbox" checked>lympics</div>
+    <div class="box1">
+      <div class="b">100 Meter Sprint</div>
+      <div class="c">
+        <div class="d">When the start</div>
+        <div class="e">board reads Go,</div>
+        <div class="f">begin clicking,</div>
+        <div class="g">speed is what</div>
+        <div class="h">matters here.</div>
+        <div class="i">Don't false start.</div>
+      </div>
+      <div class="j">
+        <div class="k"><a  href="https://checkboxolympics.com/#:~:text=Watch%20how%20on%20YouTube">Watch how on </a></div>
+        <div class="l"><a  href="https://checkboxolympics.com/#:~:text=Watch%20how%20on%20YouTube">Youtube</a></div>
+      </div>
+      <div class="m"><button>check</button></div>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+}
 </script>
+
+<style>
+body {
+  margin: 0px;
+}
+
+  .box1{
+    display: block;
+    background-color:red;
+    
+  }
+
+  .a{
+    font-size: 15px;
+    font-weight: 600;
+    letter-spacing:0px;
+    margin-top:20px;
+    margin-left: 15px;
+  }
+
+.b{
+  font-size: 12px;
+  font-weight: bold;
+  letter-spacing: -0.5px;
+  margin-left: 470px;
+  margin-top: 300px;
+}
+
+.c{
+  font-size: 13px;
+  font-weight: 100;
+  margin-left: 470px;
+  margin-top: 10px;
+}
+
+.i{
+  margin-top: 10px;
+}
+
+.j{
+  font-size: 13px;
+  margin-left: 470px;
+  }
+
+.k{
+  margin-top: 20px;
+}
+
+.l{
+  margin-top: -3px;
+}
+
+.m{
+  margin-top: 15px;
+  margin-left: 470px;
+}
+
+.n{
+  solid: #000
+}
+</style>
